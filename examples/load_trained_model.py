e3041afd92e0e3d322948aa5a806feb658f7cc12
Paul G Nelson <pgnelson307@gmail.com>
2025-06-11 13:41:02 -0400

    Create load_trained_model.py

---
+++ examples/load_trained_model.py
@@ -0,0 +1,20 @@
+import torch
+import importlib.resources as pkg_resources
+import microbiome_translator
+import microbiome_translator.resources
+import pandas as pd
+
+def example_load():
+    # Get the path to the bundled model
+    with pkg_resources.path(microbiome_translator.resources, "trained_microbiome_translator_borenstein.pth") as model_path:
+        model = load_model(model_path)
+        return model
+trained_model = example_load()
+sp_cor_df, met_cor_df = trained_model.evaluate_featurewise_correlations("cpu")
+
+sp_cor_df['Species'] = sp_cor_df.feature.str.split("s__").str[-1]
+sp_cor_df.sort_values('rho', ascending=True)
+print(sp_cor_df.head())
+
+met_cor_df.sort_values('rho', ascending=True)
+print(met_cor_df.head())
