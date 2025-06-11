868083b8881c0ddea06208de0af237c91d7b9cd7
Paul G Nelson <pgnelson307@gmail.com>
2025-06-10 22:45:40 -0400

    Update FeedForwardDecoder.py
    Added dropout layers and increased depth.

--- src/microbiome_translator/FeedForwardDecoder.py
+++ src/microbiome_translator/FeedForwardDecoder.py
@@ -2,13 +2,21 @@ class FeedForwardDecoder(nn.Module):
     def __init__(self, input_dim, output_dim):
         super().__init__()
         self.decoder = nn.Sequential(
-            nn.Linear(input_dim, 32),
+            nn.Linear(input_dim, 16),
             nn.ReLU(),
-            nn.Linear(32, 32),
+            nn.Linear(16, 16),
+            nn.Dropout(p=0.2),
             nn.ReLU(),
-            nn.Linear(32, 32),
+            nn.Linear(16, 16),
+            nn.Dropout(p=0.2),
             nn.ReLU(),
-            nn.Linear(32, output_dim)
+            nn.Linear(16, 16),
+            nn.Dropout(p=0.2),
+            nn.ReLU(),
+            nn.Linear(16, 16),
+            nn.Dropout(p=0.2),
+            nn.ReLU(),
+            nn.Linear(16, output_dim)
         )
 
     def forward(self, x):
