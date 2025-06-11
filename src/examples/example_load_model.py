27321b87545b485872a4e569c756a191899b892f
Paul G Nelson <pgnelson307@gmail.com>
2025-06-11 14:13:46 -0400

    Update example_load_model.py

--- src/examples/example_load_model.py
+++ src/examples/example_load_model.py
@@ -8,14 +8,14 @@ import pandas as pd
 def example_load():
     # Get the path to the bundled model
     with pkg_resources.path(microbiome_translator.resources, "trained_microbiome_translator_borenstein.pth") as model_path:
-        model = load_model(model_path)
-        return model
-trained_model = example_load()
-sp_cor_df, met_cor_df = trained_model.evaluate_featurewise_correlations("cpu")
+        trained_model = load_model(model_path)
+        sp_cor_df, met_cor_df = trained_model.evaluate_featurewise_correlations("cpu")
+        
+        sp_cor_df['Species'] = sp_cor_df.feature.str.split("s__").str[-1]
+        sp_cor_df.sort_values('rho', ascending=True)
+        print(sp_cor_df.head())
+        
+        met_cor_df.sort_values('rho', ascending=True)
+        print(met_cor_df.head())
+        return trained_model
 
-sp_cor_df['Species'] = sp_cor_df.feature.str.split("s__").str[-1]
-sp_cor_df.sort_values('rho', ascending=True)
-print(sp_cor_df.head())
-
-met_cor_df.sort_values('rho', ascending=True)
-print(met_cor_df.head())
