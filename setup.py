447cf346017e74735bf944acba0414581a7a5bcc
Paul G Nelson <pgnelson307@gmail.com>
2025-06-11 12:29:20 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 from setuptools import setup, find_packages
 
-setup(
-    name="microbiome_translator",
-    version="0.1.0",
-    description="Microbiome to Metabolite translator using attention-based PyTorch models",
-    author="Your Name",
-    packages=find_packages(where="src"),
+	setup(
+	name="microbiome_translator",
+	version="0.1.0",
+	description="Microbiome to Metabolite translator using attention-based PyTorch models",
+	author="Your Name",
+	packages=find_packages(where="src"),
 	package_dir={"": "src"},
-    install_requires=[
-        "torch",
-        "pandas",
-        "numpy",
-        "scipy",
-        "statsmodels",
-	"scikit-learn"
-    ],
+	install_requires=[
+		"torch",
+		"pandas",
+		"numpy",
+		"scipy",
+		"statsmodels",
+		"scikit-learn"
+	],
+	include_package_data=True,
+
 )
