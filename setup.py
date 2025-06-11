131eef19ae1ef0d083a9d641ade686bc9763a1ab
Paul G Nelson <pgnelson307@gmail.com>
2025-06-11 13:47:59 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name="microbiome_translator",
-	version="0.1.0",
+	version="0.1.1",
 	description="Microbiome to Metabolite translator using attention-based PyTorch models",
 	author="Your Name",
 	packages=find_packages(where="src"),
@@ -15,5 +15,8 @@ setup(
 		"statsmodels",
 		"scikit-learn"
 	],
-	include_package_data=True
+	include_package_data=True,
+	entry_points={
+		"console_scripts": ["load_microbiome_model = examples.example_load_model:example_load",],
+	}
 )
