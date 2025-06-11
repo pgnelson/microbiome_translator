c314b4b0fe8610f51655e603af4732c07c5171ac
Paul G Nelson <pgnelson307@gmail.com>
2025-06-11 09:10:46 -0400

    Update load_model.py
    fixed load function

--- src/microbiome_translator/load_model.py
+++ src/microbiome_translator/load_model.py
@@ -1,14 +1,19 @@
 def load_model(filepath):
     checkpoint = torch.load(filepath, weights_only = False)
-
     args = checkpoint['model_args']
     model = MicrobeMetaboliteTranslator(
         args['microbe_data'],
         args['metabolite_data'],
-        args['embed_dim'],
-        args['num_heads'],
-        args['dev_frac'],
-        args['batch_size']
+        embed_dim = args['embed_dim'],
+        num_heads = args['num_heads'],
+        dev_frac = args['dev_frac'],
+        batch_size = args['batch_size'],
+        microbe_min_frac_nonzero = 0,
+        metabolite_min_frac_nonzero = 0,
+        microbe_min_abundance = 0,
+        metabolite_min_abundance = 0,
+        scale = False,
+        renormalize = False
     )
     model.load_state_dict(checkpoint['model_state_dict'])
 
@@ -18,4 +23,3 @@ def load_model(filepath):
     model.dev_indices = checkpoint['dev_indices']
     model._build_data_loaders()
     return model
-
